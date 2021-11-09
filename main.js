e0cfc28e880ddac4fb0cd0e458451a7356e1a33c
Candy <candymdeoleo@gmail.com>
2021-11-09 16:11:08 +0100

    mas o menos

--- main.js
+++ main.js
@@ -1,25 +1,24 @@
-$(document ).ready(function() {
-    
-    const coders= [ 'Candy', 'Gabrielle', 'Carmen', 'Laura .C', 'Alexia', 
-    'Alisa', 'Kristina', 'Helen', 'Ana', 'Anna', 'Yuliya', 'Desiree', 'Laura M.', 'Valentina', 'Gracia', 
-    'Joana', 'Faby', 'Marisa', 'Judith', 'Sonia', 'Tamara', 'Rosa', 'Sandra', 'Sara', 'Marta', 'Javi',
-    ];
+$(document).ready(function () {
 
 
-    $('.start').on("click", function() {
-        
-        function elegirCoders (coders) {
-      
-      
-        
-        
-        
-          
-    
-        }
-    }
-    
+     const coders= [ 'Candy', 'Gabrielle', 'Carmen', 'Laura .C', 'Alexia', 
+     'Alisa', 'Kristina', 'Helen', 'Ana', 'Anna', 'Yuliya', 'Desiree', 'Laura M.', 'Valentina', 'Gracia', 
+     'Joana', 'Faby', 'Marisa', 'Judith', 'Sonia', 'Tamara', 'Rosa', 'Sandra', 'Sara', 'Marta', 'Javi',
+     ];
 
+     function pickCoder () {
+        let random=  Math.floor((Math.random() * coders.length));
+        var randomName=coders[random];
+        console.log(randomName);
 
+        coders.forEach( t => 
+            $('h2').append(
+                `<div class="resultado">${t}</div>`
+            )
+        );
+     
+}
+
+$('.start').on('click', pickCoder);
 
 });
