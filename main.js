89f3aac0326bd19a5b84323a60062633900167cd
Candy <candymdeoleo@gmail.com>
2021-11-09 12:58:19 +0100

    js added

---
+++ main.js
@@ -0,0 +1,25 @@
+$(document ).ready(function() {
+    
+    const coders= [ 'Candy', 'Gabrielle', 'Carmen', 'Laura .C', 'Alexia', 
+    'Alisa', 'Kristina', 'Helen', 'Ana', 'Anna', 'Yuliya', 'Desiree', 'Laura M.', 'Valentina', 'Gracia', 
+    'Joana', 'Faby', 'Marisa', 'Judith', 'Sonia', 'Tamara', 'Rosa', 'Sandra', 'Sara', 'Marta', 'Javi',
+    ];
+
+
+    $('.start').on("click", function() {
+        
+        function elegirCoders (coders) {
+      
+      
+        
+        
+        
+          
+    
+        }
+    }
+    
+
+
+
+});
